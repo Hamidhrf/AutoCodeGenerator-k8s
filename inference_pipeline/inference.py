6a6cfdbd4441f4c575e9af4e5041262dfa8a4042
BhanuPDas <bhanu.das001@stud.fh-dortmund.de>
2025-05-20 11:02:04 +0200

    Adding inference

--- inference_pipeline/inference.py
+++ inference_pipeline/inference.py
@@ -13,14 +13,14 @@ pipeline = transformers.pipeline(
 )
 
 sequences = pipeline(
-    'import socket\n\ndef ping_exponential_backoff(host: str):',
+    'write java code using springboot to receive astring message and print it.',
     do_sample=True,
     top_k=10,
     temperature=0.1,
     top_p=0.95,
     num_return_sequences=1,
     eos_token_id=tokenizer.eos_token_id,
-    max_length=200,
+    max_length=1000,
 )
 for seq in sequences:
     print(f"Result: {seq['generated_text']}")
