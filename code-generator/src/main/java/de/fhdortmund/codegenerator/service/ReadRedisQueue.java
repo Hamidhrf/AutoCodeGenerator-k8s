69d1b0a5e6766be4abadee0dbc1bd6273014dd81
Bhanu Pratap Das <bhanupd199@gmail.com>
2025-12-09 16:24:55 +0100

    Adding Intermediate Service

--- code-generator/src/main/java/de/fhdortmund/codegenerator/service/ReadRedisQueue.java
+++ code-generator/src/main/java/de/fhdortmund/codegenerator/service/ReadRedisQueue.java
@@ -34,6 +34,7 @@ public class ReadRedisQueue implements Runnable {
     private final GenerateMetrics metrics;
     private final WriteJavaFiles jfiles;
     private final InferenceRepository irepo;
+    private volatile boolean isRunning = true;
     Logger logger = LogManager.getLogger(ReadRedisQueue.class);
 
     public ReadRedisQueue(UnifiedJedis jedis, String groupName, String consumer,
@@ -55,61 +56,19 @@ public class ReadRedisQueue implements Runnable {
         logger.info("Fetching queued prompts from Redis..");
         try {
             XReadGroupParams params = new XReadGroupParams().count(100).block(5000);
+            Map<String, StreamEntryID> bID = new HashMap<>();
+            bID.put(streamKey, new StreamEntryID("0-0"));
+            processStreamMessage(jedis.xreadGroup(groupName, consumer, params, bID));
             Map<String, StreamEntryID> mID = new HashMap<>();
             mID.put(streamKey, StreamEntryID.XREADGROUP_UNDELIVERED_ENTRY);
-            List<Map.Entry<String, List<StreamEntry>>> records = jedis.xreadGroup(groupName, consumer, params, mID);
-            List<InferenceEntity> iEntityList = new ArrayList<>();
-            if (records != null) {
-                for (Map.Entry<String, List<StreamEntry>> streamData : records) {
-                    String stream = streamData.getKey();
-                    logger.info("Fetching {} record from redis...", stream);
-                    List<StreamEntry> entries = streamData.getValue();
-                    for (StreamEntry entry : entries) {
-                        StreamEntryID messageId = entry.getID();
-                        Map<String, String> fields = entry.getFields();
-                        String prompt = fields.get("prompt");
-                        logger.info("Received message ID = {} & Prompt: {}", messageId.toString(), prompt);
-                        Gson gson = new Gson();
-                        Prompts promptReq = gson.fromJson(prompt, Prompts.class);
-
-                        HttpHeaders headers = new HttpHeaders();
-                        headers.setContentType(MediaType.APPLICATION_JSON);
-                        InferenceRequest req = new InferenceRequest(promptReq.getPrompt());
-                        HttpEntity<InferenceRequest> inferenceEntity = new HttpEntity<>(req, headers);
-                        Instant timeStart = Instant.now();
-                        ResponseEntity<InferenceResponse> response = rest.exchange(inferenceUrl, HttpMethod.POST, inferenceEntity,
-                                InferenceResponse.class);
-                        Instant timeEnd = Instant.now();
-                        double latency = (Duration.between(timeStart, timeEnd).toMillis()) / 1000.0;
-                        logger.info("Latency of the request is: {} seconds", latency);
-                        if (response.getBody() != null) {
-                            String result = response.getBody().getResult();
-                            float inferenceTime = response.getBody().getInferenceTime();
-                            float tpms = response.getBody().getTpms();
-                            float nTokens = response.getBody().getNTokens();
-                            float gpuUtil = response.getBody().getGpuUtil();
-                            float memUtil = response.getBody().getMemUtil();
-                            float memUsed = response.getBody().getMemUsed();
-                            float totalMem = response.getBody().getTotalMem();
-                            logger.info("Inference response is: {} \n Latency: {} \n Inference time: {} \n Tokens per sec: {} \n No of Tokens generated: {} \n GPU Util%: {} \n Mem Engine Util%: {} \n Mem Used: {} \n Total Mem: {}",
-                                    result, latency, inferenceTime, tpms, nTokens, gpuUtil, memUtil, memUsed, totalMem);
-                            metrics.updateMetrics(latency, inferenceTime, tpms, nTokens, gpuUtil, memUtil, memUsed, totalMem);
-                            jfiles.saveFormattedJavaFile(promptReq.getId(), result);
-                            InferenceEntity iEntity = new InferenceEntity();
-                            iEntity.setMsgId(messageId.toString());
-                            iEntity.setPrompt(promptReq.getPrompt());
-                            iEntity.setResult(result);
-                            iEntity.setSentToUi("No");
-                            iEntityList.add(iEntity);
-                        }
-
-                        jedis.xack(streamKey, groupName, messageId);
-                    }
-                }
+            while(isRunning) {
+                List<Map.Entry<String, List<StreamEntry>>> records = jedis.xreadGroup(groupName, consumer, params, mID);
+                processStreamMessage(records);
             }
-            saveResultInDB(iEntityList);
+
         } catch (Exception e) {
             logger.error("Error while fetching the prompt: {}", e.getMessage());
+            isRunning = false;
         }
     }
 
@@ -121,5 +80,58 @@ public class ReadRedisQueue implements Runnable {
         } catch (Exception e) {
             logger.error("Error while saving results to DB: {}", e.getMessage());
         }
+    }
+
+    private void processStreamMessage(List<Map.Entry<String, List<StreamEntry>>> records) throws Exception{
+        List<InferenceEntity> iEntityList = new ArrayList<>();
+        if (records != null) {
+            for (Map.Entry<String, List<StreamEntry>> streamData : records) {
+                String stream = streamData.getKey();
+                logger.info("Fetching {} record from redis...", stream);
+                List<StreamEntry> entries = streamData.getValue();
+                for (StreamEntry entry : entries) {
+                    StreamEntryID messageId = entry.getID();
+                    Map<String, String> fields = entry.getFields();
+                    String prompt = fields.get("prompt");
+                    logger.info("Received message ID = {} & Prompt: {}", messageId.toString(), prompt);
+                    Gson gson = new Gson();
+                    Prompts promptReq = gson.fromJson(prompt, Prompts.class);
+
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setContentType(MediaType.APPLICATION_JSON);
+                    InferenceRequest req = new InferenceRequest(promptReq.getPrompt());
+                    HttpEntity<InferenceRequest> inferenceEntity = new HttpEntity<>(req, headers);
+                    Instant timeStart = Instant.now();
+                    ResponseEntity<InferenceResponse> response = rest.exchange(inferenceUrl, HttpMethod.POST, inferenceEntity,
+                            InferenceResponse.class);
+                    Instant timeEnd = Instant.now();
+                    double latency = (Duration.between(timeStart, timeEnd).toMillis()) / 1000.0;
+                    logger.info("Latency of the request is: {} seconds", latency);
+                    if (response.getBody() != null) {
+                        String result = response.getBody().getResult();
+                        float inferenceTime = response.getBody().getInferenceTime();
+                        float tpms = response.getBody().getTpms();
+                        float nTokens = response.getBody().getNTokens();
+                        float gpuUtil = response.getBody().getGpuUtil();
+                        float memUtil = response.getBody().getMemUtil();
+                        float memUsed = response.getBody().getMemUsed();
+                        float totalMem = response.getBody().getTotalMem();
+                        logger.info("Inference response is: {} \n Latency: {} \n Inference time: {} \n Tokens per sec: {} \n No of Tokens generated: {} \n GPU Util%: {} \n Mem Engine Util%: {} \n Mem Used: {} \n Total Mem: {}",
+                                result, latency, inferenceTime, tpms, nTokens, gpuUtil, memUtil, memUsed, totalMem);
+                        metrics.updateMetrics(latency, inferenceTime, tpms, nTokens, gpuUtil, memUtil, memUsed, totalMem);
+                        jfiles.saveFormattedJavaFile(promptReq.getId(), result);
+                        InferenceEntity iEntity = new InferenceEntity();
+                        iEntity.setMsgId(messageId.toString());
+                        iEntity.setPrompt(promptReq.getPrompt());
+                        iEntity.setResult(result);
+                        iEntity.setSentToUi("No");
+                        iEntityList.add(iEntity);
+                    }
+
+                    jedis.xack(streamKey, groupName, messageId);
+                }
+            }
+        }
+        saveResultInDB(iEntityList);
     }
 }
